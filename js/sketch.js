ccd811de81261801eb3d7cbb344c2aebd4275dca
Simon Pinkas <simon@code-think.com>
2020-06-09 15:08:14 +0200

    AudioFile Amplitude

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,31 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let audioFile;
+let amp;
+
+function preload() {
+  audioFile = loadSound('assets/audio/schmidt.mp3');
 }
 
-function draw() { 
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.mousePressed(toggleSound);
+  amp = new p5.Amplitude();
+}
 
+
+function draw() {
+  background(255);
+  let level = amp.getLevel();
+  ellipse(width / 2, height / 2, level * 1000);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function toggleSound() {
+  if (audioFile.isPlaying()) {
+    audioFile.stop();
+  } else {
+    audioFile.play();
+  }
 }
